7ebf4e721c1d2ef8facc5b354d4111414d4d83b8
MlodyJacky <source056@gmail.com>
2025-07-26 17:17:59 +0200

    seeing models on users accounts

--- user/views.py
+++ user/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .models import Profile
 from .forms import ProfileUpdateForm
+from item.models import Item
 
 @login_required
 def profile_view(request, username):
     profile = get_object_or_404(Profile, user__username=username)
+    items = Item.objects.filter(created_by=profile.user)
 
     if request.user == profile.user:
         if request.method == 'POST':
@@ -18,4 +20,8 @@ def profile_view(request, username):
     else:
         form = None  # Inni użytkownicy nie mogą edytować
 
-    return render(request, 'user/profile.html', {'profile': profile, 'form': form})
+    return render(request, 'user/profile.html', {
+        'profile': profile,
+        'form': form,
+        'items': items,
+    })
